ff9ab3e19a42f56d39e63ea73c0432a8aae2020a
aejmcclelland <aejmcclelland@googlemail.com>
2024-08-19 21:43:11 +0100

    test checking only usr can add card

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -56,8 +56,10 @@ class CashCardController {
         return ResponseEntity.ok(page.getContent());
     }
     @PutMapping("/{requestedId}")
-    private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate) {
-        // just return 204 NO CONTENT for now.
+    private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal) {
+        CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
+        cashCardRepository.save(updatedCashCard);
         return ResponseEntity.noContent().build();
     }
 }
